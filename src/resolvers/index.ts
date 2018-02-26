cdd6ab086ccf20a516e2dc0d373e2ab08232c3aa
Berezovyy Vasyl <berezoviy.vasil@gmail.com>
2018-02-26 18:56:54 +0200

    Added subscriptions for product update

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,8 +3,15 @@ import { auth } from "./Mutation/auth";
 import { post } from "./Mutation/post";
 import { product } from "./Mutation/product";
 import { AuthPayload } from "./AuthPayload";
+import { Context } from "../utils";
 
 export default {
+  Subscription: {
+    product: {
+      subscribe: async (parent, args, ctx: Context, info) =>
+        ctx.db.subscription.product({}, info)
+    }
+  },
   Query,
   Mutation: {
     ...auth,
